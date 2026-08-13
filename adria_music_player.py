ce5ff0214cf8ca2ad70940bd85a8245591e9e5c6
AdriaBC06 <adriabc06@gmail.com>
2026-08-14 01:43:59 +0200

    chore: add GPL license header to main module

--- adria_music_player.py
+++ adria_music_player.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+#
+# Copyright (C) 2026 Adrià
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version. See <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 """Adria Music Player: floating music player for GTK4/libadwaita on Wayland.
 
 Architecture:
